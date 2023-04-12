7a29b9a4c4f8b24ab010ecf8b256e0d1be818a2b
Tentaro Kubo <39017717+OriginalTT@users.noreply.github.com>
2023-04-11 21:46:08 -0400

    Create components

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { SearchBar } from "../SearchBar/SearchBar"
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+        <SearchBar />
+        <div class="App-playlist">
+          {/*<!-- Add a SearchResults component -->
+          <!-- Add a Playlist component -->*/}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
